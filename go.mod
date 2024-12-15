7b925316a13d0295f2256c8dc7604677acef6ec9
Aziret Alisher <aziretalisher@gmail.com>
2024-12-16 01:27:54 +0600

    Add tusd package from documentation

--- go.mod
+++ go.mod
@@ -5,11 +5,14 @@ go 1.22.3
 require (
 	github.com/golang-migrate/migrate/v4 v4.18.1
 	github.com/joho/godotenv v1.5.1
+	github.com/mattn/go-sqlite3 v1.14.24
+	github.com/tus/tusd/v2 v2.6.0
 )
 
 require (
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	github.com/tus/lockfile v1.2.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
+	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df // indirect
 )
--- internal/adapters/api/file/service.go
+++ internal/adapters/api/file/service.go
@@ -1,8 +1,48 @@
 package file
 
+import (
+	"net/http"
+
+	"github.com/tus/tusd/v2/pkg/filelocker"
+	"github.com/tus/tusd/v2/pkg/filestore"
+	tusd "github.com/tus/tusd/v2/pkg/handler"
+
+	"log/slog"
+)
+
 type Implementation struct {
 }
 
-func NewImplementation() *Implementation {
+func NewImplementation(logger *slog.Logger) *Implementation {
+
+	store := filestore.New("./uploads")
+	locker := filelocker.New("./uploads")
+	composer := tusd.NewStoreComposer()
+	store.UseIn(composer)
+	locker.UseIn(composer)
+
+	handler, err := tusd.NewHandler(tusd.Config{
+		BasePath:              "/files/",
+		StoreComposer:         composer,
+		NotifyCompleteUploads: true,
+	})
+	if err != nil {
+		logger.Error("unable to create handler: %s", err)
+	}
+
+	go func() {
+		for {
+			event := <-handler.CompleteUploads
+			logger.Info("Upload %s finished\n", event.Upload.ID)
+		}
+	}()
+
+	http.Handle("/files/", http.StripPrefix("/files/", handler))
+	http.Handle("/files", http.StripPrefix("/files", handler))
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		logger.Error("unable to listen: %s", err)
+	}
+
 	return &Implementation{}
 }
