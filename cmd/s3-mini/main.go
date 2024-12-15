239cce5f1704c353a24b3c1476b03c7b3ba411ae
Aziret Alisher <aziretalisher@gmail.com>
2024-12-15 16:39:49 +0600

    Setup logger

---
+++ cmd/s3-mini/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	env := os.Getenv("ENV")
+	log := setupLogger(env)
+	log.Debug("debug messages enables")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelDebug},
+			),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelDebug},
+			),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelInfo},
+			),
+		)
+	}
+
+	return log
+}
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+}
