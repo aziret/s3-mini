0b8afcd059817fdf5b3ff718f1f6e651f9e2017c
Aziret Alisher <aziretalisher@gmail.com>
2024-12-16 22:04:19 +0600

    Add migration for files table and migrator command itself

--- internal/adapters/repository/db/migrations/20241215164156_create_files_table.up.sql
+++ internal/adapters/repository/db/migrations/20241215164156_create_files_table.up.sql
@@ -2,8 +2,10 @@ CREATE TABLE IF NOT EXISTS files(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL,
   file_path TEXT,
-  checksum TEXT NOT NULL UNIQUE
+  upload_id TEXT NOT NULL UNIQUE,
+  offset INTEGER,
+  filetype TEXT
 );
 
 CREATE INDEX IF NOT EXISTS idx_file_name ON files(name);
-CREATE UNIQUE INDEX IF NOT EXISTS idx_file_checksum ON files(checksum);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_file_upload_id ON files(upload_id);
